c5f890a94b60a0e46d29f4a83c33016d50341110
mmeyrat <maximemeyrat@orange.fr>
2022-08-01 23:03:10 +0200

    move files to folders

--- scripts/background.js
+++ scripts/background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status === "complete" && tab.url === "https://www.twitch.tv/aruten_") {
 		chrome.scripting.executeScript({
 			target: { tabId: tabId },
-			files: ["./foreground.js"]
+			files: ["./scripts/foreground.js"]
 		}).then(() => {
 			console.log("foreground script started");
 		}).catch(err => console.log(err));
