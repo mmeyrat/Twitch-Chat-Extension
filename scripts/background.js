755b954c39f79dce5ff741ef94596081a2cce751
mmeyrat <maximemeyrat@orange.fr>
2022-09-07 14:51:33 +0200

    add back chrome manifest

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,15 @@
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+let api;
+
+if (typeof chrome !== "undefined" && typeof chrome.runtime !== "undefined") {
+	api = chrome;
+}
+else if (typeof browser !== "undefined" && typeof browser.runtime !== "undefined") {
+	api = browser;
+}
+
+api.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status === "complete" && tab.url === "https://www.twitch.tv/aruten_") {
-		browser.scripting.executeScript({
+		api.scripting.executeScript({
 			target: { tabId: tabId },
 			files: ["./scripts/foreground.js"]
 		}).then(() => {
